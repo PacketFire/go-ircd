4900e88dba24c39c2c04883f7236b120108bf741
mischief <mischief@offblast.org>
2013-10-10 03:45:09 -0700

    add stashing to pre-commit, so we don't try to test unstaged code

--- pre-commit.sh
+++ pre-commit.sh
@@ -5,11 +5,16 @@
 # fmt and test everything. check for garbage being comitted.
 # stash changes so only staged code is tested.
 #
-# fix stashing
 # add a check for bullshit like 'panic()' being comitted
 #
 
-#git stash -q --keep-index
+# stash only if we have unstaged changes, pop at exit if so
+if ! git diff-files --quiet
+then
+    # There are unstaged changes; stash and then pop when script exits
+    git stash -q --keep-index
+    trap 'git stash pop -q' EXIT 
+fi
 
 go build >/dev/null 2>&1
 if [ $? -ne 0 ]
@@ -17,8 +22,6 @@ then
   echo "Failed to build project. Please check the output of"
   echo "go build or run commit with --no-verify if you know"
   echo "what you are doing."
-
-#  git stash pop -q
 
   exit 1
 fi
@@ -29,8 +32,6 @@ then
   echo "Failed to run tests. Please check the output of"
   echo "go test or run commit with --no-verify if you know"
   echo "what you are doing."
-
-#  git stash pop -q
 
   exit 1
 fi
@@ -43,8 +44,6 @@ then
   echo "or run commit with --no-verify if you know what you"
   echo "are doing."
 
-#  git stash pop -q
-
   exit 1
 fi
 
@@ -55,11 +54,8 @@ then
   echo "Please check its output or run commit with --no-verify"
   echo "if you know what you are doing."
 
-#  git stash pop -q
-
   exit 1
 fi
 
 echo "ALL OK"
-#git stash pop -q
 
