8256e28fcc0e9d4779f3064937ec264378bbeaff
mischief <mischief@offblast.org>
2013-10-10 19:57:02 -0700

    fix parser again, more error checking
    add more tests for parser, thanks @grawity
    
    remove : from pong reply

--- parser/message.go
+++ parser/message.go
@@ -1,8 +1,13 @@
+// IRC Protocol message marshaller and unmarshaller.
+// refer to http://tools.ietf.org/html/rfc1459#section-2.3.1 for
+// information on the format of protocol messages
+// TODO(mischief): add IRCv3.2 tags as per http://ircv3.org/specification/message-tags-3.2
 package parser
 
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 type Message struct {
@@ -16,38 +21,36 @@ func (m Message) String() string {
 }
 
 func (m *Message) UnmarshalText(b []byte) error {
-
-	var s [][]byte
-
-	if b[0] == ':' {
-		s = bytes.SplitN(b[1:], []byte{' '}, 2)
-		m.Prefix = string(s[0])
-		b = s[1]
+	if len(b) < 1 {
+		/*  Empty  messages  are  silently  ignored, which
+				    permits use of the sequence CR-LF between messages
+		        without extra problems. */
+		return nil
 	}
 
-	s = bytes.SplitN(b, []byte{' '}, 2)
-	m.Command = string(s[0])
-	b = s[1]
+	b = bytes.TrimSpace(b)
 
-	for {
-		if b[0] == ':' {
-			m.Args = append(m.Args, string(b))
-			break
-		} else {
-			s = bytes.SplitN(b, []byte{' '}, 2)
+	if len(b) <= 0 {
+		return fmt.Errorf("short line")
+	}
 
-			if len(s) == 0 {
-				break
-			}
-
-			m.Args = append(m.Args, string(s[0]))
-
-			if len(s) < 2 {
-				break
-			}
-
-			b = s[1]
+	if b[0] == ':' {
+		split := bytes.SplitN(b, []byte{' '}, 2)
+		if len(split) <= 1 {
+			return fmt.Errorf("short line")
 		}
+		m.Prefix = string(split[0][1:])
+		b = split[1]
+	}
+	split := bytes.SplitN(b, []byte{':'}, 2)
+	args := bytes.Split(bytes.TrimSpace(split[0]), []byte{' '})
+	m.Command = string(bytes.ToUpper(args[0]))
+	m.Args = make([]string, 0, len(args))
+	for _, arg := range args[1:] {
+		m.Args = append(m.Args, string(arg))
+	}
+	if len(split) > 1 {
+		m.Args = append(m.Args, string(split[1]))
 	}
 
 	return nil
@@ -59,13 +62,24 @@ func (m *Message) MarshalText() (text []byte, err error) {
 	if len(m.Prefix) > 0 {
 		b.WriteByte(':')
 		b.WriteString(m.Prefix)
-		b.WriteByte(' ')
 	}
+
+  if len(m.Prefix) > 0 && len(m.Command) > 0 {
+		b.WriteByte(' ')
+  }
 
 	b.WriteString(m.Command)
 
-	for _, arg := range m.Args {
+	for i, arg := range m.Args {
 		b.WriteByte(' ')
+		if strings.Index(arg, " ") >= 0 || strings.Index(arg, ":") == 0 {
+			if i == len(m.Args)-1 {
+				b.WriteByte(':')
+			} else {
+				// a parameter has a space or : and it's not the last arg - wrong
+				return nil, fmt.Errorf("space or : in arg %d %q", i, arg)
+			}
+		}
 		b.WriteString(arg)
 	}
 
--- parser/message_test.go
+++ parser/message_test.go
@@ -1,12 +1,19 @@
+// Mesage marshall/unmarshall tests
+//
+// TODO(mischief): add more tests from https://raw.github.com/grawity/code/master/lib/test-irc-join.txt
 package parser
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 )
 
 type mtest struct {
-	line []byte
-	msg  Message
+	line        []byte
+	msg         Message
+	shouldFail  bool
+	skipMarshal bool
 }
 
 var (
@@ -14,28 +21,160 @@ var (
 
 		mtest{
 			[]byte(":nick!user@host.com PRIVMSG #42o3L1t3 :l0l sw4g 0m9 s0 c00l 42o"),
-			Message{"nick!user@host.com", "PRIVMSG", []string{"#42o3L1t3", ":l0l sw4g 0m9 s0 c00l 42o"}},
+			Message{"nick!user@host.com", "PRIVMSG", []string{"#42o3L1t3", "l0l sw4g 0m9 s0 c00l 42o"}},
+			false,
+			false,
 		},
 
 		mtest{
 			[]byte("COMMAND :no arguments here!"),
-			Message{"", "COMMAND", []string{":no arguments here!"}},
+			Message{"", "COMMAND", []string{"no arguments here!"}},
+			false,
+			false,
 		},
 
 		mtest{
 			[]byte("COMMAND nothing but arguments!"),
 			Message{"", "COMMAND", []string{"nothing", "but", "arguments!"}},
+			false,
+			false,
 		},
+
+		// empty - valid per rfc
+		mtest{
+			[]byte(""),
+			Message{"", "", nil},
+			false,
+			false,
+		},
+
+		// empty prefix - short message
+		mtest{
+			[]byte(":"),
+			Message{":", "", nil},
+			true,
+			true,
+		},
+
+		// prefix, no command - short message
+		mtest{
+			[]byte(":foo"),
+			Message{"foo", "", nil},
+			true,
+			false,
+		},
+
+		/* no tags for now */
+		/*
+		   mtest{
+		     []byte("@foo"),
+		     Message{"@foo", "", []string{""}},
+		     false,
+		   },
+
+		   mtest{
+		     []byte("@foo :bar"),
+		     Message{"", "", []string{""}},
+		     false,
+		   },
+		*/
+
+		mtest{
+			[]byte("foo bar baz asdf"),
+			Message{"", "foo", []string{"bar", "baz", "asdf"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte("foo bar baz :asdf quux"),
+			Message{"", "foo", []string{"bar", "baz", "asdf quux"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte("foo bar baz"),
+			Message{"", "foo", []string{"bar", "baz"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte("foo bar baz ::asdf"),
+			Message{"", "foo", []string{"bar", "baz", ":asdf"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte(":test foo bar baz asdf"),
+			Message{"test", "foo", []string{"bar", "baz", "asdf"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte(":test foo bar baz :asdf quux"),
+			Message{"test", "foo", []string{"bar", "baz", "asdf quux"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte(":test foo bar baz"),
+			Message{"test", "foo", []string{"bar", "baz"}},
+			false,
+			false,
+		},
+
+		mtest{
+			[]byte(":test foo bar baz ::asdf"),
+			Message{"test", "foo", []string{"bar", "baz", ":asdf"}},
+			false,
+			false,
+		},
+
+    mtest{
+      []byte(":foo bar"),
+      Message{"foo", "bar", nil},
+      false,
+      false,
+    },
+
+    mtest{
+      []byte(":foo :bar baz"),
+      Message{"foo", "", []string{"bar baz"}},
+      false,
+      false,
+    },
+
+    mtest{
+      []byte(":foo :bar baz"),
+      Message{"foo", "", []string{"bar baz"}},
+      false,
+      false,
+    },
+
+    mtest{
+      []byte(":foo :bar baz"),
+      Message{"foo", "", []string{"bar baz"}},
+      false,
+      false,
+    },
 	}
 )
 
 func TestUnmarshalText(t *testing.T) {
-
 	for _, ut := range mtests {
 		var m Message
 
 		if err := m.UnmarshalText(ut.line); err != nil {
-			t.Fatalf("unmarshal failed: %s", err)
+			if ut.shouldFail {
+				t.Logf("expected failure: on %q: %s", ut.line, err)
+				continue
+			}
+			t.Fatalf("unmarshal failed on %q: %s", ut.line, err)
 		}
 
 		t.Logf("unmarshal %q got %#v", ut.line, m)
@@ -44,7 +183,7 @@ func TestUnmarshalText(t *testing.T) {
 			t.Fatalf("Bad prefix")
 		}
 
-		if m.Command != ut.msg.Command {
+		if m.Command != strings.ToUpper(ut.msg.Command) {
 			t.Fatalf("Bad command")
 		}
 
@@ -57,6 +196,30 @@ func TestUnmarshalText(t *testing.T) {
 			if arg != ut.msg.Args[i] {
 				t.Fatalf("Bad argument %v", i)
 			}
+		}
+	}
+}
+
+func TestMarshalMessage(t *testing.T) {
+	for _, mt := range mtests {
+		if mt.skipMarshal {
+			continue
+		}
+
+		line, err := mt.msg.MarshalText()
+
+		if err != nil && mt.shouldFail {
+			t.Logf("expected failure on %s: %s", mt.msg, err)
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("marshal %s failed: %s", mt.msg, err)
+			continue
+		}
+
+		if bytes.Compare(line, mt.line) != 0 {
+			t.Errorf("different lines: expected %q got %q", mt.line, line)
 		}
 	}
 }
